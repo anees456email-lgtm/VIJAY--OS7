cf4e97ebaec7dbbd8c6c9b91976b9fbc18665867
anees456email-lgtm <anees456email@gmail.com>
2026-09-12 20:48:32 +0530

    Day 6: For loop se even scanner + green hacker output 📱💚

--- datafile.py
+++ datafile.py
@@ -1,7 +1,4 @@
-count = 1
-while count <= 5:
-    if count == 3:
-        count += 1
-        continue
-    print(count)
-    count += 1
+for number in range(1,21):
+    if number % 2 == 0:
+        print (f"\033[1;3;32m {number} \033[0m")
+
