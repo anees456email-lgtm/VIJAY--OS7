c6a437900d0dcab7485b45ebbe37b98cae38a19e
anees456email-lgtm <anees456email@gmail.com>
2026-09-12 12:12:24 +0530

    Day 5 ka code

--- datafile.py
+++ datafile.py
@@ -1,10 +1,7 @@
-password = 7676
-attempts = 0
-while attempts < 3:
-    i = int(input("entar password: "))
-    if i == password:
-        print (f"\033[1;3;32m Access granted {i} welcome bs\033[0m")
-        break
-    else:
-        print (f"\033[1;91m Access decline invlide user {attempts+1}:attempts left\033[0m")
-        attempts += 1
+count = 1
+while count <= 5:
+    if count == 3:
+        count += 1
+        continue
+    print(count)
+    count += 1
