03e766d7676bcbc2458c70336a4f08d0980116e4
anees456email-lgtm <anees456email@gmail.com>
2026-09-13 08:11:42 +0530

    Day 7 ka code

--- datafile.py
+++ datafile.py
@@ -1,4 +1,37 @@
-for number in range(1,21):
-    if number % 2 == 0:
-        print (f"\033[1;3;32m {number} \033[0m")
+# "Username: bilal, Password: 1234 - ye demo login hai"
+# This is for learning purposes only. No real passwords have been used in this nor has any attempt been made to try it on any real system.
+print("\033[1;31m")
+print("""
+╔══════════════════════════════════════╗
+║                                      ║
+║          ☠  BILAL TERMINAL  ☠       ║
+║                                      ║
+║        ⚠  SECURE SYSTEM  ⚠          ║
+║                                      ║
+╚══════════════════════════════════════╝
+""")
+print("\033[1;32m        >>> SYSTEM READY <<<\033[0m")
+print()
+correct_username = "bilal"
+user_pass = 1234
+attempts = 1
+while attempts <= 3:
+    input_username = input("Entar name: ")
+    user_pass = int(input("Entar pass: "))
+
+    if input_username == correct_username:
+        print (f"\033[1;3;32m username found {input_username} welcome\033[0m")
+
+        if user_pass == 1234:
+            print (f"\033[1;3;32m {user_pass} correct password login Successful\033[0m")
+            break
+        else:
+            print (f"\033[1;3;31m Wrong password attempts {attempts}/3 \033[0m")
+            attempts += 1
+    else:
+        print (f"\033[1;3;31m user not found attempts {attempts}/3 \033[0m")
+        attempts += 1
+print()
+if attempts == 4:
+    print ("\033[1;3;33m [!] Account Locked [!] \033[0m")
 
