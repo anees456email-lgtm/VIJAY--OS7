93d6b8aa8371d8d120ebe1b4769c29c2cfcd0b55
anees456email-lgtm <anees456email@gmail.com>
2026-09-15 05:06:32 +0530

    Day 10 ka code

--- datafile.py
+++ datafile.py
@@ -1,15 +1,15 @@
-# Day 9 - Even / Odd Filter with Continue
-# Challenge: 5 inputs, print only odd > 5, skip even with continue
-
-count = 1
-while count <= 5:
-    num = int(input("Enter num: "))
-
-    if num % 2 == 0:
-        print (f"\033[1;3;32m {num} \033[0m")
-        count += 1
-        continue
-
-    if num > 5:
-        print(f"\033[1;3;31m odd {num}\033[0m")
-    count += 1
+Y="\033[93m";R="\033[91m";B="\033[1m";W="\033[0m"
+print(f"""{B}{Y}
+ ╔══════════════════════════════╗
+ ║ {R}⚠️  Power of while ⚠️{Y}  ║
+ ║ {W}ACCESS DENIED = TRY HARDER{Y}  ║
+ ╚══════════════════════════════╝{W}
+""")
+print()
+while True:
+    user_input = input("Enter password: ")
+    if user_input == "bs313":
+        print (f"\033[1;3;4;32m[✓]Access granted {user_input}: correct password\033[0m")
+        break
+    else:
+        print ("\033[1;31m[×]wrong password access decline[×]\033[0m")
