01a66908d2c3325f7262a83515a73bdb4a6d20c9
anees456email-lgtm <anees456email@gmail.com>
2026-09-14 07:11:29 +0530

    Day 8 to Day 9 - updated comment

--- datafile.py
+++ datafile.py
@@ -1,12 +1,15 @@
-# Only education purpose
-# Python while loop challenge: Skip even numbers with continue, print only odd numbers > 5, ensure counter increases every round. Debugged indentation bug causing infinite loop on Termux.
+# Day 9 - Even / Odd Filter with Continue
+# Challenge: 5 inputs, print only odd > 5, skip even with continue
+
 count = 1
 while count <= 5:
-    num = int(input("Enter num:"))
+    num = int(input("Enter num: "))
+
     if num % 2 == 0:
-        print (f"\033[1;3;32m {num}\033[0m")
+        print (f"\033[1;3;32m {num} \033[0m")
         count += 1
         continue
+
     if num > 5:
-        print (f"\033[1;3;31m {num}\033[0m")
+        print(f"\033[1;3;31m odd {num}\033[0m")
     count += 1
