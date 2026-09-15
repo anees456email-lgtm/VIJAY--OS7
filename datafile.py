55546a1914646dd08cdf1c17bab6b1e56aabc105
anees456email-lgtm <anees456email@gmail.com>
2026-09-15 07:39:35 +0530

    Day 11 ka code

--- datafile.py
+++ datafile.py
@@ -1,15 +1,9 @@
-Y="\033[93m";R="\033[91m";B="\033[1m";W="\033[0m"
-print(f"""{B}{Y}
- ╔══════════════════════════════╗
- ║ {R}⚠️  Power of while ⚠️{Y}  ║
- ║ {W}ACCESS DENIED = TRY HARDER{Y}  ║
- ╚══════════════════════════════╝{W}
-""")
-print()
-while True:
-    user_input = input("Enter password: ")
-    if user_input == "bs313":
-        print (f"\033[1;3;4;32m[✓]Access granted {user_input}: correct password\033[0m")
-        break
-    else:
-        print ("\033[1;31m[×]wrong password access decline[×]\033[0m")
+# BS313 Round Tracker
+# 3 Round System | Teen Round
+# Har input ka hisaab
+# Green Underline Swag
+count = 1
+while count <= 3:
+    num = int(input("Enter num: "))
+    print(f"\033[1;3;4;32m {num} entered number: {count} round \033[0m")
+    count += 1
