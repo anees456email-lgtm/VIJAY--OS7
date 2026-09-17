a962e1f1cdc7f50259eeba896e70cc51b7ecee02
anees456email-lgtm <anees456email@gmail.com>
2026-09-17 21:46:57 +0530

    Day 15 ka code Modulo + Elif

--- datafile.py
+++ datafile.py
@@ -1,30 +1,15 @@
-# This is solely for educational purposes; no real-world assets have been used, nor have any real systems been targeted.
-import time
-
-C = "\033[96m"
-G = "\033[92m"
-R = "\033[91m"
-Y = "\033[93m"
-W = "\033[0m"
-
-banner = f"""
-{C}▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-{C}█ {G} [+] ADVANCED PORT SCANNER v1.0       {C} █
-{C}█ {Y} [!] WARNING: HONEYPOT DETECTION ON   {C} █
-{C}▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀{W}
-"""
-print(banner)
-time.sleep(1)
-
-
-port = 1
-while port <= 100:
-    if port == 22:
-        print (f"\033[1;3;4;32m port {port}: ssh is open\033[0m")
-    elif port == 80:
-        print (f"\033[1;3;4;32m port {port}: http is open\033[0m")
-    elif port == 99:
-        print (f"\033[1;91m[!] port {port}: HONEYPOT DETECTED! DISCONNECTING IMMEDIATELY[!]\033[0m")
-        break
-    port += 1
-    time.sleep(0.01)
+# This is just a simple Python script to learn.
+print ("==============================")
+print ("\033[1;3;4;36m Modulo + if elif + while\033[0m")
+print ("===============================")
+print()
+attempts = 1
+while attempts <= 3:
+    code = int(input("Enter valid code: "))
+    if code % 5 == 0 and code % 8 == 0:
+        print ("\033[1;92m[✓]Super admin! access[✓]\033[0m")
+    elif code % 5 == 0:
+        print ("\033[1;93m[~]Normal user! access[~]\033[0m")
+    else:
+        print (f"\033[1;91m[×]Access Decline[×] {attempts}/3 \033[0m")
+        attempts += 1
