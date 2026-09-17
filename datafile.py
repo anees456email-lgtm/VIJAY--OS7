c2fc7aad9531f54b7a3da607f29b269d41e62b6c
anees456email-lgtm <anees456email@gmail.com>
2026-09-17 08:01:23 +0530

    Day 13 ka code

--- datafile.py
+++ datafile.py
@@ -1,36 +1,31 @@
-# No actual passwords have been used here; this is solely for learning purposes, and no attempt has been made to test it on a real system.
+# This code does not use any real-world elements, nor does it target any real systems or servers; it is simply a basic Python script intended for learning purposes.
 import time
 
-R = "\033[1;31m"
-C = "\033[1;36m"
-Y = "\033[1;33m"
+C = "\033[96m"
+G = "\033[92m"
+R = "\033[91m"
+Y = "\033[93m"
 W = "\033[0m"
 
-
-banner = f"""{R}
-     _  _   _   ___ _  _____ ___ 
-    | || | /_\\ / __| |/ / __| _ \\
-    | __ |/ _ \\ (__| ' <| _||   /
-    |_||_/_/ \\_\\___|_|\\_\\___|_|_\\
-{C}
-╔══════════════════════════════════╗
-║ {Y}   {C}
-║ {Y}[!] STATUS: SYSTEM LOCKED        {C}║
-╚══════════════════════════════════╝{W}
+banner = f"""
+{C}╔══════════════════════════════════════════╗
+{C}║ {G} [+] SYSTEM LOGIN - SECURITY GATEWAY    {C} ║
+{C}║ {Y} [!] WARNING: MAX 3 ATTEMPTS ALLOWED    {C} ║
+{C}╚══════════════════════════════════════════╝{W}
 """
-
 print(banner)
 time.sleep(1)
-print(f"{Y}[*] Initiating Brute-Force Attack...{W}\n")
-print()
+
 attempts = 1
-secret_pin = 7744
+secret_password = "root"
 while attempts <= 3:
-    user_pass = int(input("Entar Password: "))
-    if user_pass == secret_pin:
-        print (f"\033[1;3;4;32m[✓]Access Granted! {user_pass}: Root shell open[✓]\033[0m")
+    pwd = input("Enter password: ")
+    if pwd == "root":
+        print (f"\033[1;3;4;32m [*] password entered :{pwd}: is correct [*]\033[0m")
         break
     else:
-        print (f"\033[1;91m[×]Intrusion Detected! worng pin {attempts}/3 [×]\033[0m")
+        print (f"\033[1;3;91m[×]wrong password[×] {attempts}/3 attempts \033[0m")
         attempts += 1
         time.sleep(1)
+else:
+    print ("\033[1;3;33m [!]ACCOUNT LOCKED! TO MANY FAILED ATTEMPTS[!]\033[0m")
