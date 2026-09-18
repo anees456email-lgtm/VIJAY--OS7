2058e84da63cf66263d7404ad844ad334003109f
anees456email-lgtm <anees456email@gmail.com>
2026-09-18 06:21:24 +0530

    Day 17 ka code retry_attempt_system pin

--- datafile.py
+++ datafile.py
@@ -1,8 +1,10 @@
 # For learning purposes only
 attempt = 1
 while attempt <= 3:
-    user_input = input("Enter current language: ")
-    if user_input == "python":
+    user_input = int(input("Enter pin: "))
+    if user_input == 246:
         print (f"\033[1;3;32m[✓] {user_input} is correct[✓]\033[0m")
         break
+    else:
+        print (f"\033[1;91m[×]Wrong pin {attempt}/3 attempt\033[0m")
     attempt += 1
