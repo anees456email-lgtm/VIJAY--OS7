4c95d3cec94bc064c90f0a73223998a1998d37ec
anees456email-lgtm <anees456email@gmail.com>
2026-09-18 06:11:40 +0530

    Day 16 ka code retry_attempt_system

--- datafile.py
+++ datafile.py
@@ -1,15 +1,8 @@
-# This is just a simple Python script to learn.
-print ("==============================")
-print ("\033[1;3;4;36m Modulo + if elif + while\033[0m")
-print ("===============================")
-print()
-attempts = 1
-while attempts <= 3:
-    code = int(input("Enter valid code: "))
-    if code % 5 == 0 and code % 8 == 0:
-        print ("\033[1;92m[✓]Super admin! access[✓]\033[0m")
-    elif code % 5 == 0:
-        print ("\033[1;93m[~]Normal user! access[~]\033[0m")
-    else:
-        print (f"\033[1;91m[×]Access Decline[×] {attempts}/3 \033[0m")
-        attempts += 1
+# For learning purposes only
+attempt = 1
+while attempt <= 3:
+    user_input = input("Enter current language: ")
+    if user_input == "python":
+        print (f"\033[1;3;32m[✓] {user_input} is correct[✓]\033[0m")
+        break
+    attempt += 1
