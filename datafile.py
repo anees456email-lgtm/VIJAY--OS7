c76a1f31bfcd6efa701b2d2c57771d7451a1fac7
anees456email-lgtm <anees456email@gmail.com>
2026-09-19 07:40:54 +0530

    Day 18 ka code Menu-based loops

--- datafile.py
+++ datafile.py
@@ -1,10 +1,18 @@
-# For learning purposes only
-attempt = 1
-while attempt <= 3:
-    user_input = int(input("Enter pin: "))
-    if user_input == 246:
-        print (f"\033[1;3;32m[✓] {user_input} is correct[✓]\033[0m")
+print ("==============================")
+print ("\033[1;96m Menu-based loops\033[0m")
+print ("==============================")
+print()
+while True:
+    choice = int(input("Enter Choice: "))
+    if choice == 1:
+        print (f"\033[1;4;32m[✓] choice {choice}:  System chek complete[✓]\033[0m")
+    elif choice == 2:
+        print (f"\033[1;4;32m[✓] choice {choice}:  Security chek complete[✓]\033[0m")
+    elif choice == 3:
+        print (f"\033[1;4;33m[~] choice {choice}:  System status active[~]\033[0m")
+    elif choice == 4:
+        print (f"\033[1;3;4;34m[*] choice {choice}:  System shutdown[*]\033[0m")
         break
     else:
-        print (f"\033[1;91m[×]Wrong pin {attempt}/3 attempt\033[0m")
-    attempt += 1
+        print ("\033[1;91m[×]Invlid choice[×]\033[0m")
+
