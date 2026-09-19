bf5d65c719b08fc7779b34ac4fbdbe8ff09ae50f
anees456email-lgtm <anees456email@gmail.com>
2026-09-19 21:01:28 +0530

    Day 19 ka code Added yes/no/exit logic in main menu nested loof with while

--- datafile.py
+++ datafile.py
@@ -1,18 +1,21 @@
 print ("==============================")
-print ("\033[1;96m Menu-based loops\033[0m")
+print ("\033[1;96m  menu choices \033[0m")
 print ("==============================")
 print()
 while True:
-    choice = int(input("Enter Choice: "))
-    if choice == 1:
-        print (f"\033[1;4;32m[✓] choice {choice}:  System chek complete[✓]\033[0m")
-    elif choice == 2:
-        print (f"\033[1;4;32m[✓] choice {choice}:  Security chek complete[✓]\033[0m")
-    elif choice == 3:
-        print (f"\033[1;4;33m[~] choice {choice}:  System status active[~]\033[0m")
-    elif choice == 4:
-        print (f"\033[1;3;4;34m[*] choice {choice}:  System shutdown[*]\033[0m")
-        break
+    choice = input("Enter Choice: ")
+    if choice == "1":
+        print (f"\033[1;4;32m[✓] choice {choice}: system status:ONLINE[✓]\033[0m")
+    elif choice == "2":
+        print (f"\033[1;4;32m[✓] choice {choice}:  security scan:COMPLETE [✓]\033[0m")
+    elif choice == "3":
+        confirm = input("confirm reset? (yes/no/exit): ")
+        if confirm == "yes":
+            print (f"\033[1;3;33m[✓] confirm {confirm}: session reset [✓]\033[0m")
+        elif confirm == "no":
+            print (f"\033[1;3;34m[!] confirm {confirm}: Reset cancelled[!]\033[0m")
+        elif confirm == "exit":
+            print (f"\033[1;3;4;35m[~] confirm {confirm}: Console closed[~]\033[0m")
+            break
     else:
-        print ("\033[1;91m[×]Invlid choice[×]\033[0m")
-
+        print ("\033[1;91m[×]Invalid confimation[×]\033[0m")
