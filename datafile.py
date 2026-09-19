6fddd00554c1b999198bacac4c0ac6796e3cd357
anees456email-lgtm <anees456email@gmail.com>
2026-09-20 05:19:46 +0530

    Day 20 ka code menu Menu-based loop upgraded

--- datafile.py
+++ datafile.py
@@ -9,13 +9,15 @@ while True:
     elif choice == "2":
         print (f"\033[1;4;32m[✓] choice {choice}:  security scan:COMPLETE [✓]\033[0m")
     elif choice == "3":
-        confirm = input("confirm reset? (yes/no/exit): ")
+        confirm = input("confirm reset? (yes/no/): ")
         if confirm == "yes":
             print (f"\033[1;3;33m[✓] confirm {confirm}: session reset [✓]\033[0m")
         elif confirm == "no":
             print (f"\033[1;3;34m[!] confirm {confirm}: Reset cancelled[!]\033[0m")
-        elif confirm == "exit":
-            print (f"\033[1;3;4;35m[~] confirm {confirm}: Console closed[~]\033[0m")
-            break
+        else:
+            print ("\033[1;91m[×]Invalid confimation[×]\033[0m")
+    elif choice == "4":
+        print ("\033[1;3;4;35m[~]Console closed[~]\033[0m")
+        break
     else:
-        print ("\033[1;91m[×]Invalid confimation[×]\033[0m")
+        print ("\033[1;91m[×]Invalid Choice[×]\033[0m")
