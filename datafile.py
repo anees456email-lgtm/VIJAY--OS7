3ba9bee49b499cf90cfaf58d3009fc185acb8e05
anees456email-lgtm <anees456email@gmail.com>
2026-09-21 07:02:41 +0530

    Day 21 ka code infinity loop press ctrl+c loop stop

--- datafile.py
+++ datafile.py
@@ -1,23 +1,6 @@
-print ("==============================")
-print ("\033[1;96m  menu choices \033[0m")
-print ("==============================")
-print()
-while True:
-    choice = input("Enter Choice: ")
-    if choice == "1":
-        print (f"\033[1;4;32m[✓] choice {choice}: system status:ONLINE[✓]\033[0m")
-    elif choice == "2":
-        print (f"\033[1;4;32m[✓] choice {choice}:  security scan:COMPLETE [✓]\033[0m")
-    elif choice == "3":
-        confirm = input("confirm reset? (yes/no/): ")
-        if confirm == "yes":
-            print (f"\033[1;3;33m[✓] confirm {confirm}: session reset [✓]\033[0m")
-        elif confirm == "no":
-            print (f"\033[1;3;34m[!] confirm {confirm}: Reset cancelled[!]\033[0m")
-        else:
-            print ("\033[1;91m[×]Invalid confimation[×]\033[0m")
-    elif choice == "4":
-        print ("\033[1;3;4;35m[~]Console closed[~]\033[0m")
-        break
-    else:
-        print ("\033[1;91m[×]Invalid Choice[×]\033[0m")
+count = 3
+while count >= 1:
+    count += 1
+    print (f"\033[1;32m {count}\033[0m")
+
+print ("\033[1;91m Program end\033[0m")
