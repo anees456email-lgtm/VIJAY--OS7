55de1772ae5d9a34d88a5c7ce4793890979d34fa
anees456email-lgtm <anees456email@gmail.com>
2026-09-21 15:07:45 +0530

    Day 22 ka code  Created a Fake SSH Brute-Forcer with terminal color formatting and time delay logic.

--- datafile.py
+++ datafile.py
@@ -1,6 +1,15 @@
-count = 3
-while count >= 1:
-    count += 1
-    print (f"\033[1;32m {count}\033[0m")
-
-print ("\033[1;91m Program end\033[0m")
+# No real passwords have been used here, nor has it been tested on any real system; this is just a simple Python script created solely for learning purposes.
+import time
+print ("====================================")
+print ("\033[1;3;36m The Fake SSH Brute-Forcer\033[0m")
+print ("====================================")
+print()
+target_password = "admin"
+password_list = ["root","7766","password123","admin@123","admin"]
+for p in password_list:
+    if p == target_password:
+        print (f"\033[1;3;4;32m[✓]password cracked :{p}:\033[0m")
+        break
+    else:
+        print (f"\033[1;91m[×]Invild password {p}:[×]\033[0m")
+        time.sleep(1)
