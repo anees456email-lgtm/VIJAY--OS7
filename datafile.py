c06e9fbf2d126528d94302180a807ce734e690fe
anees456email-lgtm <anees456email@gmail.com>
2026-09-22 22:30:47 +0530

    Day 24 ka code funny ABCD...to z

--- datafile.py
+++ datafile.py
@@ -1,17 +1,3 @@
-# No real passwords have been used here, nor has it been tested on any real system; this is just a simple Python script created solely for learning purposes.
-import time
-print ("====================================")
-print ("\033[1;3;36m The 3-Strike Vault\033[0m")
-print ("====================================")
-print()
-secret_password = "password123"
-count = 1
-while count <= 3:
-    user_input = input("Enter password: ")
-    if user_input == secret_password:
-        print (f"\033[1;3;4;32m[✓] {user_input}: Access granted [✓]\033[0m")
-        break
-    else:
-        print (f"\033[1;91m[×]Access Decline[×] {count}/3 count \033[0m")
-        count += 1
-        time.sleep(1)
+while True:
+    print ("\033[1;3;4;32mA,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z",end=" ")
+
