d7f3ff21a10e88c725505862f935eab6b1a652e0
anees456email-lgtm <anees456email@gmail.com>
2026-09-24 07:16:42 +0530

    Day 26 ka code using with rockyou.txt Added Python script to parse large wordlists with line tracking, error bypass, and ethical disclaimer

--- datafile.py
+++ datafile.py
@@ -1,11 +1,11 @@
-import time
+# No attempt has been made here to crack actual ID passwords, nor has this been tested on a real system; instead, a publicly available text file is used. This Python script demonstrates how a `for` loop handles such a large text file.
+
+file = open("rockyou.txt","r")
 count = 1
-while count <= 5:
-    if count == 3:
-        print (f"\033[1;32m {count} Checkpoint\033[0m")
-    else:
-        print (f"\033[1;91m {count} Round\033[0m")
+for i in file:
+    print ("\033[1;3;33m Checking: " + i)
+    if i == "i love you\n":
+        print (f"\033[1;3;4;32m {i} password found {count}\033[0m")
+        break
     count += 1
-    time.sleep(1)
-print ("\033[1;93m Program Finished\033[0m")
 
