ac93b00143e9033872f50a2abdd7058927bd2254
anees456email-lgtm <anees456email@gmail.com>
2026-09-24 21:46:18 +0530

    Day 27 ka code fake port scanner

--- datafile.py
+++ datafile.py
@@ -1,11 +1,13 @@
-# No attempt has been made here to crack actual ID passwords, nor has this been tested on a real system; instead, a publicly available text file is used. This Python script demonstrates how a `for` loop handles such a large text file.
-
-file = open("rockyou.txt","r")
-count = 1
-for i in file:
-    print ("\033[1;3;33m Checking: " + i)
-    if i == "i love you\n":
-        print (f"\033[1;3;4;32m {i} password found {count}\033[0m")
-        break
-    count += 1
-
+# This does not target a real system in any way; rather, it is a straightforward and well-structured Python script created solely for educational purposes.
+import time
+print ("======================================")
+print ("\033[1;3;35m FAKE PORT SCANNER\033[0m")
+print ("--------------------------------------")
+print()
+ports = [22,80,443,445,8080]
+for port in ports:
+    if port == 445:
+        print (f"\033[1;3;4;31m[!] Warning: port {port}:  is open\033[0m")
+    else:
+        print (f"\033[1;92m[✓] {port} port scanned status secure [✓]\033[0m")
+        time.sleep(1)
