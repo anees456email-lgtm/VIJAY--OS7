cbf6667c0e6bfe5c579412f1ef227949bdbc17e0
anees456email-lgtm <anees456email@gmail.com>
2026-09-25 07:45:40 +0530

    Day 28 ka code fake packet sender

--- datafile.py
+++ datafile.py
@@ -1,13 +1,17 @@
-# This does not target a real system in any way; rather, it is a straightforward and well-structured Python script created solely for educational purposes.
+# This is merely a fake packet scanner; this script does not target any real systems and was created solely for educational purposes.
 import time
 print ("======================================")
-print ("\033[1;3;35m FAKE PORT SCANNER\033[0m")
-print ("--------------------------------------")
+print ("\033[1;3;36m Fake packet_sender\033[0m")
+print ("======================================")
 print()
-ports = [22,80,443,445,8080]
-for port in ports:
-    if port == 445:
-        print (f"\033[1;3;4;31m[!] Warning: port {port}:  is open\033[0m")
+packet = 0
+while packet < 10:
+    packet += 1
+    if packet == 4 or packet == 7:
+        print (f"\033[1;91m[!]packet {packet}: corrupted skipping...\033[0m")
+        continue
     else:
-        print (f"\033[1;92m[✓] {port} port scanned status secure [✓]\033[0m")
+        print (f"\033[1;3;4;32m[+]packet {packet}: sent successfully[+]\033[0m")
         time.sleep(1)
+print()
+print ("\033[1;93m[✓]All data transfer complete[✓]\033[0m")
