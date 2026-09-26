a024434709f04b9667bce8824acae029a431229a
anees456email-lgtm <anees456email@gmail.com>
2026-09-26 08:02:58 +0530

    Day 30 ka code my own fake mini DirBuster

--- datafile.py
+++ datafile.py
@@ -1,13 +1,16 @@
 # this script does not target any real systems and was created solely for educational purposes.
 import time
 print ("======================================")
-print ("\033[1;3;35m Brute-Force PIN Cracker Simulator\033[0m")
+print ("\033[1;3;34m Mini DirBuster Simulator\033[0m")
 print ("======================================")
 print()
-target_pin = 8426
-for i in range(1,9001):
-    print (f"\033[1;93m [*] Trying pin: {i}\033[0m")
-    if i == target_pin:
-        print (f"\033[1;3;4;32m[✓]pin cracked! the pin is {i}\033[0m")
+target_url = "[www.target.com](https://www.target.com)"
+worldlist = ["/contact","/images","/robots.txt","/adminl_panel","/congig.php"]
+for path in worldlist:
+    print (f"\033[1;93m[*] Scanning: {target_url} {path}\033[0m")
+    if path == "/adminl_panel":
+        print (f"\033[1;3;4;32m[✓]BOOM! hidden {path}: found\033[0m")
         break
+    else:
+        print ("\033[1;91m[!]404 not found[!]\033[0m")
         time.sleep(1)
